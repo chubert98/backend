d0cd603bf26036de095082faf30abaee0336e167
CHubert <carloshubert98@gmail.com>
2025-06-23 15:46:26 -0300

    part3 section d
    commiting project to test notes application fully on the internet through Render

--- index.js
+++ index.js
@@ -53,12 +53,6 @@ app.delete('/api/notes/:id', (request, response, next) => {
 app.post('/api/notes', (request, response, next) => {
   const body = request.body
 
-  if (!body.content){
-    return response.status(400).json({
-      error: 'content missing'
-    })
-  }
-
   const note = new Note({
     content: body.content,
     important: body.important || false,
@@ -176,6 +170,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id'})
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message})
   }
 
   next(error)
