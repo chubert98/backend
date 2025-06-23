a25665fe183b704351bca2d247f5ccc1ea3f486e
CHubert <carloshubert98@gmail.com>
2025-06-23 16:48:42 -0300

    part3 up to 3.21
    part3 done up to exercise 3.21, commiting project to test application fully on the internet through Render

--- index.js
+++ index.js
@@ -118,7 +118,9 @@ app.post('/api/people', (request, response, next) => {
     .then(savedPerson => {
       response.json(savedPerson)
     })
-    .catch(error => next(error))
+    .catch(error => {
+      next(error)
+    })
 })
 
 app.delete('/api/people/:id', (request, response, next) => {
@@ -171,7 +173,7 @@ const errorHandler = (error, request, response, next) => {
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id'})
   } else if (error.name === 'ValidationError') {
-    return response.status(400).json({ error: error.message})
+    return response.status(400).send({ error: error.message})
   }
 
   next(error)
